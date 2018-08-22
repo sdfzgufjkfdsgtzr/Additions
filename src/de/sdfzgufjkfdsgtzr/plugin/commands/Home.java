4aae354bde33eabad6e92641e999e5d8f33397ee
sdfzgufjkfdsgtzr <sdfzgufjkfdsgtzr@users.noreply.github.com>
2018-08-22 09:47:28 +0200

    Added World support to home command
    Signed-off-by: sdfzgufjkfdsgtzr <sdfzgufjkfdsgtzr@users.noreply.github.com>

--- src/de/sdfzgufjkfdsgtzr/plugin/commands/Home.java
+++ src/de/sdfzgufjkfdsgtzr/plugin/commands/Home.java
@@ -4,11 +4,11 @@ import de.sdfzgufjkfdsgtzr.plugin.Main;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
 import java.io.File;
@@ -35,7 +35,7 @@ public class Home implements CommandExecutor {
                 if (args.length == 0) {
                     int coords[] = getHome(p.getUniqueId());
                     if (coords != null) {
-                        Location home = new Location(p.getWorld(), coords[0], coords[1], coords[2]);
+                        Location home = new Location(Bukkit.getWorld(homes.getString("users." + p.getUniqueId().toString() + ".world")), coords[0], coords[1], coords[2]);
                         p.teleport(home);
                         p.sendMessage(ChatColor.GRAY + lang.getString(plugin.lang + ".home.teleport-message"));
                         return true;
@@ -48,8 +48,8 @@ public class Home implements CommandExecutor {
                         int x = p.getLocation().getBlockX();
                         int y = p.getLocation().getBlockY();
                         int z = p.getLocation().getBlockZ();
-                        setHome(p.getUniqueId(), x, y, z);
-                        String message = String.format(lang.getString(plugin.lang + ".home.set"), x, y, z);
+                        setHome(p.getUniqueId(), p.getWorld(), x, y, z);
+                        String message = String.format(lang.getString(plugin.lang + ".home.set"), x, y, z, p.getWorld().getName());
                         p.sendMessage(ChatColor.GRAY + message);
                         return true;
                     }
@@ -69,10 +69,11 @@ public class Home implements CommandExecutor {
     }
 
 
-    private void setHome(UUID uuid, int x, int y, int z){
+    private void setHome(UUID uuid, World world, int x, int y, int z){
         homes.set("users." + uuid.toString() + ".x", x);
         homes.set("users." + uuid.toString() + ".y", y);
         homes.set("users." + uuid.toString() + ".z", z);
+        homes.set("users." + uuid.toString() + ".world", world.getName());
         plugin.saveConfigFile(homes, plugin.home_file);
     }
 
